6b8d76e2f41ab6ef5ae66bd398409bb3633508a8
Alex Collins <alextcollins@gmail.com>
2012-05-08 21:32:18 +0100

    Add basic unit tests

--- test/lix/test/core.clj
+++ test/lix/test/core.clj
@@ -2,5 +2,14 @@
   (:use [lix.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest lix-yields-zeros
+  (is (= 0 (lix "")))
+  (is (= 0 (lix "123456nowordsorpunctuation"))))
+
+(deftest lix-yeilds-non-zeros
+  (is (< 0 (lix "Hello there, this is some good text.")))
+  (is (< 0 (lix "Another sentence."))))
+
+(deftest lix-score-matches-expected
+  (is (= 1 (lix "Hello there."))); 2/2 + (0 * 100)/2 = 1
+  (is (= 2.333333333 (lix "Hello there, this is some good text.")))) ; 7/3 + (0 * 100)/7 = 2.333333333
