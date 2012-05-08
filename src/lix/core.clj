7da7a259614bd72386a75b9d01e758fa226bc703
Alex Collins <alextcollins@gmail.com>
2012-05-08 21:32:02 +0100

    Basic functions

--- src/lix/core.clj
+++ src/lix/core.clj
@@ -1,5 +1,28 @@
-(ns lix.core
-    :gen-class)
+(ns lix.core)
 
-(defn -main [a1] 
-  (println "hi" a1))
+;From http://en.wikipedia.org/wiki/LIX
+;
+;LIX = A/B + (C x 100)/A, where
+; A = Number of words
+; B = Number of periods (defined by period, colon or capital first letter)
+; C = Number of long words (More than 6 letters)
+
+(defn num-words 
+  "Count the number of words in the given string s"
+  [s] 
+  ())
+
+(defn num-long-words 
+  ""
+  [s] 
+  ())
+
+(defn num-periods
+  "Count the number of periods -- full-stop, colon or capital first letter -- in the given string s"
+  [s] 
+  ())
+
+(defn lix
+  "Calculate the LIX score for the given string s"
+  [s]
+  ())
