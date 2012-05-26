f9240251c9d6ac32af8fef1a0ff7d417bd9b1e1f
Alex Collins <alextcollins@gmail.com>
2012-05-26 14:42:13 +0100

    add tests for helper functions

--- test/lix/test/core.clj
+++ test/lix/test/core.clj
@@ -13,3 +13,15 @@
 (deftest lix-score-matches-expected
   (is (= 1 (lix "Hello there."))); 2/2 + (0 * 100)/2 = 1
   (is (= 2.333333333 (lix "Hello there, this is some good text.")))) ; 7/3 + (0 * 100)/7 = 2.333333333
+
+(deftest is-long-word-yields-1
+  (is (= 1 (is-long-word "long-view")))
+  (is (= 1 (is-long-word "Antidisestablishmentarianism")))
+  (is (= 1 (is-long-word "slurp-tastic")))
+  (is (= 1 (is-long-word "smashing!"))))
+
+(deftest is-long-word-yields-0
+  (is (= 0 (is-long-word "hi")))
+  (is (= 0 (is-long-word "")))
+  (is (= 0 (is-long-word "wee")))
+  (is (= 0 (is-long-word "!"))))
