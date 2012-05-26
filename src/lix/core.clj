21093d3df479893804880de0ed9d5ed3166f8000
Alex Collins <alextcollins@gmail.com>
2012-05-26 14:41:45 +0100

    Split functions out further; add private naive lix func

--- src/lix/core.clj
+++ src/lix/core.clj
@@ -1,5 +1,5 @@
-(ns lix.core
-  :use cloure.string)
+(ns lix.core)
+  ;(:use clojure.string))
 
 ;From http://en.wikipedia.org/wiki/LIX
 ;
@@ -23,13 +23,19 @@
   [s] 
   ())
 
-(defn naive-lix
+(defn is-long-word 
+  "return 1 if long word, 0 otherwise"
+  [w]
+  (if (> (count w) 6) 1 0)); could've done something with floor(- 6 len)?
+
+(defn- naive-lix
+  "A basic, naive implementation of LIX (private)"
   [s]
   (let
-    [wds (clojure.string/split s #"\s+")]
+    [wds (split s #"\s+")]
     (println wds)))
 
 (defn lix
   "Calculate the LIX score for the given string s"
   [s]
-  ())
+  (naive-lix s))
