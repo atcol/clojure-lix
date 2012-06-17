c96c966353e36af054a042b43ab3d64f287eaa01
Alex Collins <alextcollins@gmail.com>
2012-06-17 11:41:48 +0100

    Use chars instead of strings

--- src/lix/core.clj
+++ src/lix/core.clj
@@ -8,17 +8,22 @@
 ; B = Number of periods (defined by period, colon or capital first letter)
 ; C = Number of long words (More than 6 letters)
 
+(defn is-capital?
+  "Return true if the char c's ascii value is > 64 and less than 91"
+  [c]
+  (and (> (int c) 64) (< (int c) 91)))
+
 (defn is-period?
   [c]
-  "Return true if the given one-char-string is a . , or capital letter"
+  "Return true if the given one-char-string is a . or ,"
   (if (= "" c) 
     false
-    (or (= "." c) (= "," c) (and (> (int (.charAt c 0)) 64) (< (int (.charAt c 0)) 91)))))
+    (or (= \. c) (= \, c))))
 
 (defn num-periods
-  "Count the number of periods -- full-stop, colon or capital first letter -- in the given string s"
-  [s] 
-  (count (filter is-period? (map str (seq s)))))
+  "Count the number of periods -- full-stop, colon or capital first letter -- in the given word w"
+  [w] 
+  (+ (if (is-capital? (first w)) 1 0) (count (filter is-period? (rest w))))) ; first filter is passing chars: why?
 
 (defn is-long-word 
   "return 1 if long word, 0 otherwise"
@@ -26,13 +31,14 @@
   (if (> (count w) 6) 1 0))
 
 (defn- naive-lix
-  "A basic, naive implementation of LIX (private)"
+  "A basic, naive implementation of LIX (private); assumes s is not empty"
   [s]
-  (loop
-    [words (clojure.string/split s #"\s+") n-wds (count words) lng-wds 0 n-pds 0]
-    (if (empty? words)
-      (+ (/ n-wds n-pds) (/ (* lng-wds 100) n-wds))
-      (recur (rest words) (count (rest words)) (+ lng-wds (is-long-word (first words))) (+ n-pds (num-periods (first words)))))))
+  ())   ; num words is constant, num periods might be zero 
+  ;(loop
+    ;[words (clojure.string/split s #"\s+") n-wds (count words) lng-wds 0 n-pds 1]
+    ;(if (empty? words)
+      ;(+ (/ n-wds n-pds) (/ (* lng-wds 100) n-wds))
+      ;(recur (rest words) (count (rest words)) (+ lng-wds (is-long-word (first words))) (+ n-pds (num-periods (first words)))))))
 
 (defn lix
   "Calculate the LIX score for the given string s"
