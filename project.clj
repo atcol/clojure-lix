8a81c1a4a4a12ef46796c6256974d5495d994875
Alex Collins <alextcollins@gmail.com>
2012-06-17 09:58:47 +0100

    Move to clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lix "1.0.0-SNAPSHOT"
   :description "A LIX calculator"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"] [org.clojure/clojure-contrib "1.2.0"]]
   :aot [lix.core]
   :main lix.core)
